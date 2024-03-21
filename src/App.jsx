bc6b3950bfb24dc8c99c0fbe960a2024f995b2d7
EnzoAblis <112990234+EnzoAblis@users.noreply.github.com>
2024-03-20 20:48:44 -0500

    added redirects file

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/history" element={<History />} />
-          <Route path="/" element={<Shopping />} />
+          <Route path="/shopping" element={<Shopping />} />
         </Routes>
       </BrowserRouter>
     </>
